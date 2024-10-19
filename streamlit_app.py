2e3cd8946460df7ac10c690a00752a9208db144b
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-10-19 21:31:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.write("ellen24k")
+
+st.markdown("""
+    <script>
+        setTimeout(function() {
+            window.location.href = 'https://www.naver.com';
+        }, 3000); // 3초 후에 리다이렉트
+    </script>
+""", unsafe_allow_html=True)
+
+st.write("3초 후에 네이버로 이동합니다...")
