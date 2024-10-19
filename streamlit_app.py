2c38a30bd69c28d3aade7d8f023b0e84bfe3dcb5
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-10-19 21:52:24 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,5 @@
 
 import streamlit as st
 
-st.title("리다이렉트 예제")
+st.write("ellen24k")
 
-st.markdown('<a href="https://quasarzone.com/" target="_self">이동</a>', unsafe_allow_html=True)
-
-# st.write("ellen24k")
-# st.write("3초 후에 네이버로 이동합니다...")
