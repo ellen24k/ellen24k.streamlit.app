30cc46ea53cd3ae19e4b5a51dba3971379d39a95
TaeYoung Kim <161318146+ellen24k@users.noreply.github.com>
2024-10-19 21:33:37 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
+st.write("ellen24k")import streamlit as st
+
+
+
 import streamlit as st
 
+st.title("리다이렉트 예제")
 
+if st.button("네이버로 이동"):
+    st.markdown('<a href="https://www.naver.com" target="_self">네이버로 이동</a>', unsafe_allow_html=True)
 
-st.markdown("""
-    <script>
-        setTimeout(function() {
-            window.location.href = 'https://www.naver.com';
-        }, 3000); // 3초 후에 리다이렉트
-    </script>
-""", unsafe_allow_html=True)
 # st.write("ellen24k")
 # st.write("3초 후에 네이버로 이동합니다...")
